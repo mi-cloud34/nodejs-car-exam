31914501128a83115edbb2e35f90f4c85c809db2
Mesutio <mesutibisoglu@gmail.com>
2022-12-08 18:42:57 +0300

    change server

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.use(
   );
 app.get('/', (req, res)=>{
     res.sendStatus(200);
-})
+}) 
 app.listen(process.env.APP_PORT,()=>{
     console.log('sunucu ayaga kalktı');
     app.use("/cars",CarRoutes);
